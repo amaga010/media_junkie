573db673f364c2e6def312819fced18163de25d3
Rihanna <alexmagana@Alexs-MacBook-Air.local>
2019-05-08 19:18:57 -0700

    mysql database

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,20 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+CREATE DATABASE movieJunkie_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE movieJunkie_db;
+
+CREATE table surveyData
+
+(
+  id int NOT NULL AUTO_INCREMENT,
+  age int,
+  mood VARCHAR (20),
+  genre int,
+  element VARCHAR (20),
+  alone boolean,
+  discover VARCHAR (20), 
+  director int,
+  netflix VARCHAR (50),
+  hulu VARCHAR (50),
+  prime VARCHAR (50),
+  PRIMARY KEY(id)
+);
